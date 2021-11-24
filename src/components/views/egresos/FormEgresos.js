aac829066df00de50ac2b9ee92d5ea923f0eabfb
AilinCarribero <42686226+AilinCarribero@users.noreply.github.com>
2021-11-24 15:14:07 -0300

    Se modifico: css de ingresos

--- src/components/views/egresos/FormEgresos.js
+++ src/components/views/egresos/FormEgresos.js
@@ -42,8 +42,8 @@ const FormEgresos = () => {
         id_forma_pago: '',
         fecha_diferido_pago: '',
         observaciones: '',
-        id_comprobante_pago: 0,
-        numero_comprobante: 0,
+        id_comprobante_pago: '',
+        numero_comprobante: '',
         centro_costo: ''
     });
 
